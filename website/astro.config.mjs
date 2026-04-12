3770039e02d5596e96d2db17533754e24d25b69f
L-S-2020 <leostewal@gmail.com>
2026-04-12 22:35:01 +0200

    Revert "making ready for cloudflare pages"
    This reverts commit 27e7e5fb72f35ce024e9fc6b0e306fcc728faa81.

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,17 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
   image: {
     layout: 'constrained'
-  },
-
-  adapter: cloudflare()
+  }
 });
